610ea252d3b3d6b8ef5312addbe74d73eddeb5ec
Nas Shihab <nas.shihab92@gmail.com>
2022-01-16 16:22:54 +0600

    G

--- lib/login_app.dart
+++ lib/login_app.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
